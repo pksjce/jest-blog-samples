a839bd9ccf1de8cdd0f359759a05fd8a2e5f1dbd
Pavithra K <pksjce@gmail.com>
2016-11-19 15:00:42 +0530

    Improve the setup file

--- window-testing/setupFile.js
+++ window-testing/setupFile.js
@@ -1,6 +1,7 @@
-const jsdom = require('jsdom');
+import {jsdom} from 'jsdom';
+
 const documentHTML = '<!doctype html><html><body><div id="root"></div></body></html>';
-global.document = jsdom.jsdom(documentHTML);
+global.document = jsdom(documentHTML);
 global.window = document.parentWindow;
 global.window.resizeTo = (width, height) => {
 	global.window.innerWidth = width || global.window.innerWidth;
